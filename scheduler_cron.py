76f42b5b37dc3bd17da7ee9698dbbe0e5a3b3a47
MarcinPierchala <macintecmarcin@gmail.com>
2025-02-17 17:53:27 +0100

    .

--- scheduler_cron.py
+++ scheduler_cron.py
@@ -1,4 +1,9 @@
-from appFleetImpTasks import job_2, job_1
+from app import job_2, job_1
+from logevent import log_event
 
-job_1()
-job_2()
+try:
+    job_1()
+    job_2()
+    log_event("Wykonał się scheduler_cron")
+except Exception as e:
+    log_event(f"Podczas wykoywania scheduler_cron nastąpił błąd: {e}")
